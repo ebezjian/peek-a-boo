f40c012de8cbb6beee84d58e1b0392e1796c4701
Maxi Benbassat <info@benbassatdigital.com>
2020-10-20 14:22:26 -0400

    add images of sandcastles and fadin toggles, clickthrough links, css style changes to images and resizing
    Co-authored-by: Ellie Lambert <ebezjian@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,14 @@ $(document).ready(function() {
     $(".clickable").click(function() {
       $("#walrus-showing").fadeToggle();
       $("#walrus-hidden").toggle();
+     
     });
   });
+
+$(document).ready(function(){
+   $(".clickable2").click(function() {
+      $("#sandcastle-showing").fadeToggle();
+      $("#sandcastle-hidden").toggle();  
+
+    });
+});
